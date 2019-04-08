de6c989f0c6dcfa15017d56f1bffaa176802279b
Dareos Meyer <dareosmyr@gmail.com>
2019-04-08 14:46:43 -0700

    Styling finished, keyboard supported, clear/del toggle & plus/minus toggle added. ReadME added. Code cleaned (a bit).

---
+++ README.md
@@ -0,0 +1,13 @@
+This is a calculator build for The Odin Project by Dareos Meyer.
+The purpose of this project was to tie all the different elements of HTML, CSS and JS that have been covered so far in the course. Everything from styling to math functions were left up to me.
+
+This calculator can perform simple arithmetic operations including adding, subtracting, multiplying and dividing. It can both take and return integers and decimal numbers. It can parse multiple operations in a single input or one at a time. It employs both a delete and clear key which toggles depending on context. The plus/minus button works the same way. Each operator replaces the previous one when clicked in a row. This calculator does support keyboard for the number and equals keys.
+
+Things learned in this project:
+    * More CSS Grid formatting and other CSS styling techniques.
+    * RPN shunting algorithms and parsing functions.
+    * Some prototype methods.
+    * More event listener use cases.
+    * Keyboard integration.
+    * More DOM manipulation.
+    * The importance of bug testing.
--- main.js
+++ main.js
@@ -12,32 +12,7 @@ const inputCheck = /[0-9]+[.][0-9]+|[0-9]+|[.][0-9]+|[\/\-\+\*]/g;
 populateDisplay(0);
 populateResult(0);
 initKeys();
-
-function populateDisplay(item) {
-    screen.textContent = item;
-}
-
-function populateResult(item) {
-    resultScreen.textContent = item;
-}
-
-function setInput(item) {
-    input = item;
-}
-
-function concatInput(item) {
-    input += item;
-}
-
-function clearAllDisplay() {
-    screen.textContent = '';
-    resultScreen.textContent = '';
-}
-
-function clearInput() {
-    input = '';
-}
-
+//Initialize Calculator keys to take inputs and show hover/click events.
 function initKeys() {
     allKeys.forEach(key => {
         key.addEventListener('mouseover', () => {
@@ -55,7 +30,7 @@ function initKeys() {
     })
     operatorKeys.forEach(key => {
         key.addEventListener('mousedown', () => {
-            operatorToggle(key);
+            operatorDisplayToggle(key);
         })
     })
     numberKeys.forEach(key => {
@@ -68,107 +43,10 @@ function initKeys() {
             calculate(key);
         })
     })
-    window.addEventListener('keydown', test);
+    window.addEventListener('keydown', readKey);
     window.addEventListener('transitionend', removeHover);
 }
-
-function test(e) {
-    const key = document.querySelector(`div[data-key='${e.keyCode}']`);
-    if (!key) console.log(e.keyCode);
-    key.classList.add('keyed');
-}
-
-function removeHover() {
-    allKeys.forEach(key => {
-        key.classList.remove('keyed');
-    })
-}
-
-function getInput(key) {
-    if (key.innerText === '.') {
-        if (input.search(/[.]/g) == -1 || input.search(operatorCheck) > -1) {
-            populateDisplay(key.innerText);
-            setInput(key.innerText);
-        }
-    } else {
-        screen.textContent = input += key.innerText;
-        if (input.length > 9) {
-            alert('Ten digit limit.');
-        }
-    }
-}
-
-function operatorToggle(key) {
-    if (input === '') {
-        return;
-    } if (key.innerText === 'Del') {
-        setInput(input.replace(/[0-9]$|[\/\-\+\*]$/g, ''));
-        populateDisplay(input);
-    } else if (input.search(/[0-9]/g) > -1 && input.search(/[0-9]$/g) > -1) {
-        concatInput(key.textContent.trim());
-        populateDisplay(input);
-    } else if (input.search(operatorCheck) > -1) {
-        setInput(input.replace(/[\/\-\+\*]$/g, key.innerText.trim()));
-        populateDisplay(input);
-    }
-}
-
-function calculate(key) {
-    if (key.innerText === 'C') {
-        populateDisplay(0);
-        populateResult(0);
-        clearInput();
-    } else if (key.innerText === '=' && input.search(operatorCheck) > -1) {
-        shuntInput();
-        parseShunt();
-    } else if (key.innerText === '=' && input.search(operatorCheck) === -1) {
-        populateDisplay(input);
-    }
-}
-
-function shuntInput() {
-    var outputQueue = [];
-    var operatorStack =[];
-    var precedence = {
-        '*' : 3,
-        '/' : 3,
-        '+' : 2,
-        '-' : 2
-    }
-    Array.prototype.peek = function() {
-        return this.slice(-1)[0];
-    }
-
-    inputArray = input.match(inputCheck);
-    inputArray.forEach(function(value) {
-        if (parseFloat(value)) {
-            outputQueue.push(value);
-        } else {
-            while (operatorStack.peek() && precedence[value] <= precedence[operatorStack.peek()]) {
-                outputQueue.push(operatorStack.pop());
-            }
-            operatorStack.push(value);
-        }
-    })
-    return (outputQueue.concat(operatorStack.reverse()));
-}
-
-function parseShunt() {
-    let resultStack =[];
-    shuntInput().forEach(function(value){
-        if (parseFloat(value)) {
-            resultStack.push(value);
-        } else {
-            var firNum = resultStack.pop();
-            var secNum = resultStack.pop();
-            if (!parseFloat(value)) {
-                resultStack.push(operate(value, parseFloat(secNum), parseFloat(firNum)));
-            }
-        }
-    })
-    populateResult(resultStack.join(''));
-}
-
+// Math functions.
 function add(firNum, secNum) {
     return firNum + secNum;
 }
@@ -195,4 +73,217 @@ function operate(value, firNum, secNum) {
     } else if (value === '/') {
         return divide(firNum, secNum);
     } 
+}
+
+function populateDisplay(item) {
+    screen.textContent = item;
+}
+
+function populateResult(item) {
+    resultScreen.textContent = item;
+}
+
+function setInput(item) {
+    input = item;
+}
+
+function concatInput(item) {
+    input += item;
+}
+
+function clearAllDisplay() {
+    screen.textContent = '';
+    resultScreen.textContent = '';
+}
+
+function clearInput() {
+    input = '';
+}
+// Clear screen or delete a single number as well as toggle the key between C and A/C.
+function toggleClear(key) {
+    if (key.innerText === "A/C") {
+        populateDisplay(0);
+        populateResult(0);
+        clearInput();
+    } else if (key.innerText === "C") {
+        setInput(input.replace(/[\.][0-9]+$|[0-9]+[\.][0-9]+$|[0-9]+$|[\.]$/g, ''));
+        populateDisplay(input);
+        key.innerHTML = '<p>A/C</p>';
+    }
+}
+
+function toggleDel() {
+    if (input) {
+        const clear = document.querySelector('#keyClear');
+        clear.innerHTML = '<p>C<p>';
+    }
+}
+// Toggle add/subtract key between adding and subtracting.
+function toggleAdd(key) {
+    if (key.innerText === '-') {
+        key.innerHTML = '<p>+</p>';
+    } else if (key.innerText === '+') {
+        key.innerHTML = '<p>-</p>';
+    }
+    return key.innerText;
+}
+// Alert user when the character limit is reached.
+function charLimitAlert() {
+    if (input.length >= 32) {
+        alert("33 character limit");
+    }
+}
+// Format the result based on number of decimal places.
+function generateResult(item) {
+    if (Number.isInteger(Number(item))) {
+        return item;
+    } else {
+        return parseFloat(item)
+                .toFixed(4)
+                .replace(/[0]+$/g, '');
+    }
+}
+// Two functions to count number of operators and decimals in the input string.
+String.prototype.countOp = function(string) {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string.charAt(i) === '/' || string.charAt(i) === '*' || string.charAt(i) === '+' || string.charAt(i) === '-') {
+            count++;
+        }
+    }
+    return count;
+}
+
+String.prototype.countDec = function(string) {
+    let count = 0
+    for (let i = 0; i < string.length; i++) {
+        if (string.charAt(i) === '.') {
+            count++;
+        }
+    }
+    return count;
+}
+// Read input from keyboard, display on screen and store in input variable.
+// Runs calculation function if "=" is pressed.
+function readKey(e) {
+    charLimitAlert();
+    while (input.length < 32) {
+        const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+        if (!key) {
+            return;
+        }
+        key.classList.add('keyed');
+        if (key.innerText === '=') {
+            calculate(key);
+        } else {
+            concatInput(key.innerText);
+            populateDisplay(input);
+        }
+        toggleDel();
+        break;
+    }
+}
+
+function removeHover() {
+    allKeys.forEach(key => {
+        key.classList.remove('keyed');
+    })
+}
+// Read input from clicked div, display and store.
+// Prevents multiple decimals from being placed after another & prevents input after 32 characters.
+function getInput(key) {
+    charLimitAlert();
+    if (key.innerText === '.') {
+        while (input.length < 33) {
+            if (input.search(/[\.]/g) == -1 || String.prototype.countOp(input) == (String.prototype.countDec(input))) {
+                concatInput(key.innerText);
+                populateDisplay(input);
+            }
+            break;
+        }
+    } else {
+        while (input.length < 32) {
+            concatInput(key.innerText);
+            populateDisplay(input);
+            break;
+        }
+    }
+    toggleDel();
+}
+//Displays and stores operators & will toggle between operators when a new one is clicked.
+function operatorDisplayToggle(key) {
+    charLimitAlert();
+    while (input.length < 32) {
+        if (input === '') {
+            return;
+        } if (input.search(/[0-9]/g) > -1 && input.search(/[0-9]$/g) > -1) {
+            concatInput(key.textContent.trim());
+            populateDisplay(input);
+            toggleAdd(key);
+        } else if (input.search(operatorCheck) > -1) {
+            setInput(input.replace(/[\/\-\+\*]$/g, key.innerText.trim()));
+            populateDisplay(input);
+            toggleAdd(key);
+        }
+        break;
+    }
+}
+// Calculates the result of input and displays on result screen.
+function calculate(key) {
+    toggleClear(key);
+    if (key.innerText === '=' && input.search(operatorCheck) > -1) {
+        shuntInput();
+        parseShunt();
+    } else if (key.innerText === '=' && input.search(operatorCheck) === -1) {
+        populateResult(input);
+        egg();
+    }
+}
+// Takes input variable and returns in RPN format.
+function shuntInput() {
+    var outputQueue = [];
+    var operatorStack =[];
+    var precedence = {
+        '*' : 3,
+        '/' : 3,
+        '+' : 2,
+        '-' : 2
+    }
+    Array.prototype.peek = function() {
+        return this.slice(-1)[0];
+    }
+
+    inputArray = input.match(inputCheck);
+    inputArray.forEach(function(value) {
+        if (parseFloat(value)) {
+            outputQueue.push(value);
+        } else {
+            while (operatorStack.peek() && precedence[value] <= precedence[operatorStack.peek()]) {
+                outputQueue.push(operatorStack.pop());
+            }
+            operatorStack.push(value);
+        }
+    })
+    return (outputQueue.concat(operatorStack.reverse()));
+}
+//Parses RPN from shuntInput() and returns a non-formatted integer or float based on previously declaired math functions.
+function parseShunt() {
+    let resultStack =[];
+    shuntInput().forEach(function(value){
+        if (parseFloat(value)) {
+            resultStack.push(value);
+        } else {
+            var firNum = resultStack.pop();
+            var secNum = resultStack.pop();
+            if (!parseFloat(value)) {
+                resultStack.push(operate(value, parseFloat(secNum), parseFloat(firNum)));
+            }
+        }
+    })
+    populateResult(generateResult(resultStack.join('')));
+    egg();
+}
+
+function egg() {
+    if (resultScreen.innerText == '111126') alert("You have been using the 'Limited Application Mathematical Equipment'. \n\t\t\t\t Built by Dareos Meyer 4/2019");
 }
